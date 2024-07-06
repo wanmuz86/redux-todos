7a462c9b7671294e30ab7e15159503e61158b455
Wan Muzaffar <wanmuz86@gmail.com>
2024-07-06 11:32:27 +0800

    add Add component

---
+++ src/components/add.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+const Add = () => {
+    const [text, setText] = useState('')
+    const addTodo = () => {
+
+    }
+  return (
+    <div>
+        <h2>Add new Todo</h2>
+        <div>
+            <input type="text" value={text} 
+            onChange={(e)=>setText(e.target.value)}/>
+            <button onClick={addTodo}>Add To Do</button>
+        </div>
+    </div>
+  )
+}
+
+export default Add
---
+++ src/components/row.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Row = () => {
+  return (
+    <div>row</div>
+  )
+}
+
+export default Row
